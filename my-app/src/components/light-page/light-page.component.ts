610f0dddc53db44e2a2877a0cffb22156d37d80e
Loan Dumenoir <loan.dumenoir@gmail.com>
2024-03-19 11:10:46 +0100

    first commit

---
+++ my-app/src/components/light-page/light-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-light-page',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './light-page.component.html',
+  styleUrl: './light-page.component.css'
+})
+export class LightPageComponent {
+  light: boolean = false;
+
+  switchLight():void {
+    this.light = !this.light;
+  }
+}
